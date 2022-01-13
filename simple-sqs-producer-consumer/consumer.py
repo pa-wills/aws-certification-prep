53987763aa1f83f1a6180f6d28501a83b3a3aedf
Pete Wills <34256848+pa-wills@users.noreply.github.com>
2022-01-13 19:52:53 +1100

    Added ttl support to the DyDB table
    Hopefully this will stop the table to growing to such a ridiculous size. Will need to test and see.
    
    Good test of the feature as well.

--- simple-sqs-producer-consumer/consumer.py
+++ simple-sqs-producer-consumer/consumer.py
@@ -9,12 +9,14 @@ def lambda_handler(event, context):
     dydb = boto3.resource("dynamodb")
     queue_url = 'https://sqs.ap-southeast-2.amazonaws.com/916589119438/MyQueue'
     
-    response_sqs = sqs.receive_message(QueueUrl = queue_url, MaxNumberOfMessages = 1)
+    response_sqs = sqs.receive_message(AttributeNames = ["All"], QueueUrl = queue_url, MaxNumberOfMessages = 1)
     operand_1 = int(response_sqs["Messages"][0]["Body"])
     operand_2 = randrange(100)
 
     sentTimeStampEpoch = int(int(response_sqs["Messages"][0]["Attributes"]["SentTimestamp"]) / 1000)
     sentTimeStampStr = datetime.datetime.fromtimestamp(sentTimeStampEpoch)  
+
+    ttl = int(datetime.datetime.now().timestamp()) + (3600 * 2) # I.e. Now + 2 hours.
 
     table = dydb.Table("Sums")
     response_dydb = table.put_item(
@@ -22,7 +24,8 @@ def lambda_handler(event, context):
             'DateTime': str(sentTimeStampStr),
             'operand_1': operand_1,
             'operand_2': operand_2,
-            'sum': (operand_1 + operand_2)
+            'sum': (operand_1 + operand_2),
+            'ttl': ttl
         }
     )
 
