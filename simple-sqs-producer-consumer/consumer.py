c71b1dfb2020b225caa4064a33b2d2dd1d43a246
Pete Wills <34256848+pa-wills@users.noreply.github.com>
2022-01-14 12:24:57 +1100

    Directly bound the Consumer Lambda to the SQS
    I had a loose, wildcard Event Pattern previously, which was bound to get me into trouble. I don't think it was even tied to a specific Queue!
    
    Lambda Handler function re-written to use the argv, rather than to invoke the sqs api.
    
    Modified the Service Role also to allow for CloudWatch logging. This was necessary for debugging. Testing the Consumer with test events no longer works effectively because of the code no longer directly references the queue through boto3. Hence - you have to let the consumer rip, print to stdout (which results in logging), and then debug from the logs.

--- simple-sqs-producer-consumer/consumer.py
+++ simple-sqs-producer-consumer/consumer.py
@@ -5,34 +5,34 @@ import datetime
 import json
 
 def lambda_handler(event, context):
-    sqs = boto3.client("sqs")
     dydb = boto3.resource("dynamodb")
-    queue_url = 'https://sqs.ap-southeast-2.amazonaws.com/916589119438/MyQueue'
+    print("Event:" + str(event))
     
-    response_sqs = sqs.receive_message(AttributeNames = ["All"], QueueUrl = queue_url, MaxNumberOfMessages = 1)
-    operand_1 = int(response_sqs["Messages"][0]["Body"])
-    operand_2 = randrange(100)
+    for record in event['Records']:
+        print("Record:" + str(record))
+        operand_1 = int(record["body"])
+        operand_2 = randrange(100)
 
-    sentTimeStampEpoch = int(int(response_sqs["Messages"][0]["Attributes"]["SentTimestamp"]) / 1000)
-    sentTimeStampStr = datetime.datetime.fromtimestamp(sentTimeStampEpoch)  
+        sentTimeStampEpoch = int(int(record["attributes"]["SentTimestamp"]) / 1000)
+        sentTimeStampStr = datetime.datetime.fromtimestamp(sentTimeStampEpoch)  
 
-    ttl = int(datetime.datetime.now().timestamp()) + (3600 * 2) # I.e. Now + 2 hours.
+        ttl = int(datetime.datetime.now().timestamp()) + (3600 * 2) # I.e. Now + 2 hours. Row count should be < 100.
 
-    table = dydb.Table("Sums")
-    response_dydb = table.put_item(
-        Item = {
-            'DateTime': str(sentTimeStampStr),
-            'operand_1': operand_1,
-            'operand_2': operand_2,
-            'sum': (operand_1 + operand_2),
-            'ttl': ttl
+        table = dydb.Table("Sums")
+        response_dydb = table.put_item(
+            Item = {
+                'DateTime': str(sentTimeStampStr),
+                'operand_1': operand_1,
+                'operand_2': operand_2,
+                'sum': (operand_1 + operand_2),
+                'ttl': ttl
+            }
+        )
+
+        # Apparently I don't need to explicitly delete the messages. 
+        # Lambda does it for me. Assuming successful processing.
+
+#        response_sqs = sqs.delete_message(QueueUrl = queue_url, ReceiptHandle = response_sqs["Messages"][0]["ReceiptHandle"])
+        return {
+            'statusCode': 200,
         }
-    )
-
-    response_sqs = sqs.delete_message(QueueUrl = queue_url, ReceiptHandle = response_sqs["Messages"][0]["ReceiptHandle"])
-
-    return {
-        'statusCode': 200,
-        'message': json.dumps(response_sqs),
-        'sent': json.dumps(str(sentTimeStampStr)),
-    }
