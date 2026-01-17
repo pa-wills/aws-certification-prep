8288769afa76be92f345f86a696eb752b3f7962d
Pete Wills <34256848+pa-wills@users.noreply.github.com>
2026-01-17 15:39:28 +1100

    Update process.py

--- reinvent-pipeline/pipeline/process.py
+++ reinvent-pipeline/pipeline/process.py
@@ -4,7 +4,7 @@ def chunk_text(text, chunkSize = 500):
     words = text.split()
     chunks = []
     for i in range(0, len(words), chunkSize):
-        chunks.append(" ".join(words[i:i+chunk_size]))
+        chunks.append(" ".join(words[i : i + chunkSize]))
     return chunks
 
 #    with open(text_file, "r", encoding="utf-8") as f:
